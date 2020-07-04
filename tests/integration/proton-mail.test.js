09b00cfd5d8237de7794890c16e6f0c959f69f46
Justin Kalland <justin@kalland.com>
2020-07-04 17:09:31 +0200

    refactor: lint fixes for dot notation

--- tests/integration/proton-mail.test.js
+++ tests/integration/proton-mail.test.js
@@ -5,19 +5,19 @@ describe('ProtonMail', () => {
   it('gets conversation counts', async () => {
     const counts = await support.pm.getConversationCounts()
     expect(counts).to.be.a('object')
-    expect(counts['labels']).to.be.a('object')
-    expect(counts['folders']).to.be.a('object')
-    expect(counts['folders']).to.include.all.keys('inbox', 'spam', 'trash')
-    expect(counts['folders']['inbox']).to.have.all.keys('total', 'unread')
+    expect(counts.labels).to.be.a('object')
+    expect(counts.folders).to.be.a('object')
+    expect(counts.folders).to.include.all.keys('inbox', 'spam', 'trash')
+    expect(counts.folders.inbox).to.have.all.keys('total', 'unread')
   })
 
   it('gets email counts', async () => {
     const counts = await support.pm.getEmailCounts()
 
     expect(counts).to.be.a('object')
-    expect(counts['labels']).to.be.a('object')
-    expect(counts['folders']).to.be.a('object')
-    expect(counts['folders']).to.include.all.keys('inbox', 'spam', 'trash')
-    expect(counts['folders']['inbox']).to.have.all.keys('total', 'unread')
+    expect(counts.labels).to.be.a('object')
+    expect(counts.folders).to.be.a('object')
+    expect(counts.folders).to.include.all.keys('inbox', 'spam', 'trash')
+    expect(counts.folders.inbox).to.have.all.keys('total', 'unread')
   })
 })
